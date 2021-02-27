2c57a10190b8ad1a9985e5a7d099aec84595e430
Detelina <palova.detelina@gmail.com>
2021-02-28 00:28:10 +0200

    Initial commit - added headers for subjects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from '../app/main-page/main-page.component'
+import {TabMenuModule} from 'primeng/tabmenu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+    
+    items: MenuItem[];
+
+    activeItem: MenuItem;
+
+    ngOnInit() {
+        this.items = [
+            {label: 'История', icon: 'pi pi-fw pi-calendar'},
+            {label: 'География', icon: 'pi pi-fw pi-map-marker'},
+            {label: 'Математика', icon: 'pi pi-fw pi-check-square'},
+            {label: 'Български', icon: 'pi pi-fw pi-comments'},
+            {label: 'Английски', icon: 'pi pi-fw pi-globe'},
+            {label: 'Изкуство', icon: 'pi pi-fw pi-palette'},
+            {label: 'Икономика', icon: 'pi pi-fw pi-money-bill'},
+            {label: 'Химия', icon: 'pi pi-fw pi-question'},
+            {label: 'Физика', icon: 'pi pi-fw pi-directions'},
+            {label: 'Биология', icon: 'pi pi-fw pi-image'},
+            {label: 'Философия', icon: 'pi pi-fw pi-circle-off'},
+        ];
+        this.activeItem = this.items[0];
+    }
+}
